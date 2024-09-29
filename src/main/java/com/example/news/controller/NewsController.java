045e54da61538e6c024fef6396fb81875e41d496
AlexanoffUra <131712695+IntergalacticOctopus@users.noreply.github.com>
2024-09-29 17:43:12 +0300

    Test kafka

--- src/main/java/com/example/news/controller/NewsController.java
+++ src/main/java/com/example/news/controller/NewsController.java
@@ -3,15 +3,11 @@ package com.example.news.controller;
 import com.example.news.dto.NewNewsDto;
 import com.example.news.dto.NewsDto;
 import com.example.news.dto.NewsParamDto;
-import com.example.news.config.KafkaSender;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.example.news.dto.NewsDeletionEvent;
 import com.example.news.dto.UpdateNewsDto;
 import com.example.news.service.NewsService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
@@ -35,8 +31,6 @@ import java.util.List;
 public class NewsController {
 
     private final NewsService newsService;
-    private final KafkaSender kafkaSender;
-    private final ObjectMapper objectMapper;
 
     @GetMapping
     @Operation(summary = "Информация о новостях по параметрам")
@@ -99,8 +93,6 @@ public class NewsController {
     public void deleteNews(@Parameter(description = "ID новости") @PathVariable Integer news_id) throws JsonProcessingException {
         log.info("Deleting news with id {}", news_id);
         newsService.deleteNews(news_id);
-        String newsDeletionEvent = objectMapper.writeValueAsString(new NewsDeletionEvent(news_id));
-        kafkaSender.sendMessage(newsDeletionEvent, "news-deletion-topic");
         log.info("Deleted news with id {}", news_id);
     }
 }
